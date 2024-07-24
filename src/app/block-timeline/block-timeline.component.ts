6bd329e8e84630c59ce4f1d5cd916914e9708f51
JeanCarlos20-code <jeancarlos272010@gmail.com>
2024-07-23 22:46:58 -0300

    feat: add class in css

--- src/app/block-timeline/block-timeline.component.ts
+++ src/app/block-timeline/block-timeline.component.ts
@@ -1,9 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Component, ElementRef, Input, Renderer2, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-block-timeline',
   templateUrl: './block-timeline.component.html',
-  styleUrl: './block-timeline.component.scss'
+  styleUrls: ['./block-timeline.component.scss'],
+  animations: [
+    trigger('btnState', [
+      state('normal', style({
+        transform: 'scale(1)',
+      })),
+      state('active', style({
+        top: '5%',
+      })),
+      transition('normal => active', animate('100ms ease-in')),
+      transition('active => normal', animate('100ms ease-in'))
+    ])
+  ]
 })
 export class BlockTimelineComponent {
   @Input() containerClass: string = '';
@@ -11,4 +24,23 @@ export class BlockTimelineComponent {
   @Input() title: string = '';
   @Input() text: string = '';
   @Input() arrowClass: string = '';
+  @Input() numberImageTimeline: string = '';
+
+  myState = 'normal';
+  activeIndex: number | null = null;
+  indexImage: number | null = Number(this.numberImageTimeline);
+
+  @ViewChildren('img') images: QueryList<ElementRef>;
+
+  constructor(private renderer: Renderer2) { }
+
+  toggle(imageIndex: number | null): void {
+    if (this.activeIndex === imageIndex) {
+      this.activeIndex = null;
+    } else {
+      this.activeIndex = imageIndex;
+    }
+
+    console.log(this.indexImage)
+  }
 }
--- src/app/pages/timeline-principal/timeline-principal.component.ts
+++ src/app/pages/timeline-principal/timeline-principal.component.ts
@@ -1,34 +1,11 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-timeline-principal',
   templateUrl: './timeline-principal.component.html',
   styleUrl: './timeline-principal.component.scss',
-  animations: [
-    trigger('btnState', [
-      state('normal', style({
-        width: '40px',
-        transform: 'scale(1)'
-      })),
-      state('active', style({
-        width: '80px',
-        transform: 'scale(1.5)'
-      })),
-      transition('normal => active', animate('100ms ease-in')),
-      transition('active => normal', animate('100ms ease-in'))
-    ])
-  ]
+
 })
 export class TimelinePrincipalComponent {
-  myState = 'normal';
 
-  constructor() { }
-  click() {
-    console.log('click');
-  }
-
-  toggle() {
-    this.myState = (this.myState === 'normal' ? 'active' : 'normal');
-  }
 }
